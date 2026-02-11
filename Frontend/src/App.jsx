79939c50d4ad9821a30e0c432dc9a699acad8d0b
Harshal Varade <developer.ever@gmail.com>
2026-02-11 13:47:05 +0530

    Semifinal version before update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import axios from "axios"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
   const [notes, setNotes] = useState([
@@ -21,22 +21,114 @@ const App = () => {
     },
   ]);
 
-  axios.get("http://localhost:3000/api/notes")
-  .then((res)=>{
-    setNotes(res.data.notes)
-  })
+  function fetchNotes() {
+    axios.get("http://localhost:3000/api/notes").then((res) => {
+      setNotes(res.data.notes);
+    });
+  }
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const { title, description } = e.target.elements;
+
+    axios
+      .post("http://localhost:3000/api/notes", {
+        title: title.value,
+        description: description.value,
+      })
+      .then((res) => {
+        console.log(res.data);
+        fetchNotes();
+      });
+  }
+
+  function handleDelete(noteId) {
+    axios.delete("http://localhost:3000/api/notes/" + noteId).then((res) => {
+      console.log(res.data);
+      fetchNotes();
+    });
+  }
+
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const [currentNoteid, setCurrentNoteid] = useState(null);
+  const [editData, setEditData] = useState({
+    title: "",
+    description: "",
+  });
+
+  function handleUpdate(note) {
+    setCurrentNoteid(note._id);
+    setEditData({
+      title: note.title,
+      description: note.description,
+    });
+    setIsModelOpen(true);
+  }
+
+  function handleSaveUpdate() {
+    axios.patch("http://localhost:3000/api/notes/" + currentNoteid,editData).then((res)=>{
+      console.log(res.data)
+      setIsModelOpen(false)
+      fetchNotes()
+    })
+  }
+
   return (
     <>
+      <form className="note-create-form" onSubmit={handleSubmit}>
+        <input name="title" type="text" placeholder="Enter title" />
+        <input name="description" type="text" placeholder="Enter description" />
+        <button>Create Note</button>
+      </form>
+
       <div className="notes">
         {notes.map((note) => {
           return (
-            <div className="note">
+            <div className="note" key={note._id}>
               <h1>{note.title}</h1>
               <p>{note.description}</p>
+              <div className="btn">
+                <button
+                  onClick={() => {
+                    handleUpdate(note);
+                  }}
+                  className="update"
+                >
+                  <i className="ri-edit-box-line"></i>Update
+                </button>
+                <button
+                  onClick={() => {
+                    handleDelete(note._id);
+                  }}
+                  className="delete"
+                >
+                  <i className="ri-delete-bin-line"></i>Delete
+                </button>
+              </div>
             </div>
           );
         })}
       </div>
+      {
+        isModelOpen && (
+          <div className="model-layer">
+            <div className="model">
+              <h2>Edit Note</h2>
+              <input type="text" value={editData.title} onChange={(e)=>setEditData({...editData,title:e.target.value})} />
+              <input type="text" value={editData.description} onChange={(e)=>setEditData({...editData,description:e.target.value})} />
+              <div className="model-btn">
+                <button onClick={handleSaveUpdate}>Save</button>
+                <button onClick={()=>setIsModelOpen(false)}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        )
+      }
     </>
   );
 };
